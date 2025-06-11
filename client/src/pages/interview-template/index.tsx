47e72b96b3e14e3e1152ab7cffef59ad3b07f5ca
Marika Mella <marika.developer@gmail.com>
2025-06-11 22:24:24 +0200

    refactor: remove TODO comment for question bank selection in InterviewTemplate

--- client/src/pages/interview-template/index.tsx
+++ client/src/pages/interview-template/index.tsx
@@ -58,7 +58,6 @@ const InterviewTemplate = () => {
       </div>
 
       <div className='py-14'>
-        {/* TODO: can add here question bank selection */}
         <QuestionBankSelector templateId={id} />
         <div className='mt-4'>
           <QuestionCard templateId={id} />

58afc86d8b9215e3fe1292710e9b2919c74f33b5
Marika Mella <marika.developer@gmail.com>
2025-06-13 21:21:36 +0200

    feat: implement new side bar

---
+++ client/src/components/AppSidebar.tsx
@@ -0,0 +1,91 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import { useAuth } from '@/contexts/AuthContext';
+import { useNavLinks } from '@/hooks/useNavLinks';
+import { ChevronUp, User2 } from 'lucide-react';
+import { Link } from 'react-router';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+export function AppSidebar() {
+  const navLinks = useNavLinks();
+  const { user, logout } = useAuth();
+  const { state } = useSidebar();
+
+  return (
+    <Sidebar collapsible='icon'>
+      <SidebarHeader>
+        <Link
+          to='/dashboard'
+          className={`text-xl font-bold ${
+            state === 'collapsed' ? 'text-center' : ''
+          }`}>
+          {state === 'collapsed' ? 'Si' : 'Simple Interview'}
+        </Link>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navLinks.map((item) => (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton
+                    asChild
+                    isActive={window.location.pathname.includes(item.route)}>
+                    <Link to={item.route}>
+                      <item.icon />
+                      <span>{item.label}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> {user?.fullName || 'User'}
+                  <ChevronUp className='ml-auto' />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side='top'
+                className='w-[--radix-popper-anchor-width]'>
+                {/* <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem> */}
+                <DropdownMenuItem>
+                  <button onClick={logout}>Sign out</button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,14 +1,17 @@
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Outlet } from 'react-router';
-import { Header } from './Header';
+import { AppSidebar } from './AppSidebar';
 import { Toaster } from './ui/sonner';
 
 export const Layout: React.FC = () => {
   return (
-    <div className='flex min-h-screen flex-col'>
+    <SidebarProvider>
+      <AppSidebar />
       {/* Navigation */}
-      <Header />
+      {/* <Header /> */}
       {/* Main content */}
-      <main className='flex-1'>
+      <main className='grid min-h-screen w-full'>
+        <SidebarTrigger />
         <div className='min-h-screen bg-background p-8'>
           <div className='mx-auto max-w-5xl'>
             <Outlet />
@@ -16,6 +19,6 @@ export const Layout: React.FC = () => {
           </div>
         </div>
       </main>
-    </div>
+    </SidebarProvider>
   );
 };

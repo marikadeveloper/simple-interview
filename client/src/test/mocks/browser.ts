da582ca014b32b835be1590861b1fcbb243ec537
Marika Mella <marika.developer@gmail.com>
2025-07-12 11:00:17 +0200

    cleanup

--- client/src/test/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
-
-// This configures a request mocking worker with the given request handlers
-export const worker = setupWorker(...handlers);
--- client/src/test/setup.ts
+++ client/src/test/setup.ts
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
-import { afterAll, afterEach, beforeAll, vi } from 'vitest';
-import { server } from './mocks/server';
+import { vi } from 'vitest';
 
 // Mock ResizeObserver
 global.ResizeObserver = vi.fn().mockImplementation(() => ({
@@ -20,13 +19,3 @@ function defineHasPointerCapturePolyfill() {
     Element.prototype.hasPointerCapture = () => false;
   }
 }
-
-// Establish API mocking before all tests
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests
-afterEach(() => server.resetHandlers());
-
-// Clean up after the tests are finished
-afterAll(() => server.close());
